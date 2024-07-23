d1cd636de40cdb2b8270b02c69d2fd28e2356077
Shahbaz <shahbazathwal2107@gmail.com>
2024-07-23 11:55:25 +0530

    shadcn carousel added

--- src/components/FloatingNavbar.tsx
+++ src/components/FloatingNavbar.tsx
@@ -1,4 +1,4 @@
-import MenuBar from "../MenuBar"
+import MenuBar from "./MenuBar"
 
 function FloatingNavbar() {
   return (
--- src/components/RecentlyPlayed.tsx
+++ src/components/RecentlyPlayed.tsx
@@ -1,8 +1,14 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { useSwipeable } from "react-swipeable";
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 import { SpotifyPlayedItem } from "@/types/recentlyPlayed";
-import Arrow from "./icons/Arrow";
 import Link from "next/link";
 
 type RecentlyPlayedProps = {
@@ -10,80 +16,53 @@ type RecentlyPlayedProps = {
 };
 
 const RecentlyPlayed: React.FC<RecentlyPlayedProps> = ({ recentPlays }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? recentPlays.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === recentPlays.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      handleNext();
-    }, 5000);
-
-    return () => clearInterval(interval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentIndex]);
-
-  const handlers = useSwipeable({
-    onSwipedLeft: handleNext,
-    onSwipedRight: handlePrev,
-    trackMouse: true,
-  });
-
   return (
-    <div {...handlers} className="relative overflow-hidden">
-      <h1 className="font-bold text-2xl leading-tight pb-6 mx-4 md:mx-0">Recently Played</h1>
-      <div
-        className="flex transition-transform duration-500 ease-in-out"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+    <div className="relative overflow-hidden">
+      <h1 className="font-bold text-2xl leading-tight pb-6 mx-4 md:mx-0">
+        Recently Played
+      </h1>
+      <Carousel
+        className="w-full"
+        plugins={[
+          Autoplay({
+            delay: 4000,
+          }),
+        ]}
       >
-        {recentPlays.map((item) => (
-          <div key={item.playedAt} className="w-full flex-shrink-0">
-            <div className="p-4 mx-6 relative">
-              <Link
-                href={item.url!}
-                passHref
-                target="_blank"
-                rel="noopener noreferrer"
-                title={`${item.title} by: ${item.artist}`}
-                aria-label={`${item.title} by: ${item.artist}`}
-              >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={item.thumbnail}
-                  alt={item.title}
-                  className="w-full h-full sm:h-64 object-cover rounded-lg shadow-lg dark:shadow-zinc-900 brightness-[0.4]"
-                />
-                <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-                  <h3 className="text-xl font-semibold">{item.title}</h3>
-                  <p className="text-gray-300 max-w-56 sm:max-w-full text-center">{item.artist}</p>
-                </div>
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
-      <button
-        onClick={handlePrev}
-        className="absolute  top-[55%] left-0  bg-transparent -rotate-[90deg] hover:scale-125 transition-transform duration-300"
-      >
-        <Arrow />
-      </button>
-      <button
-        onClick={handleNext}
-        className="absolute top-[55%] right-0 transform  bg-transparent rotate-90 hover:scale-125 transition-transform duration-300"
-      >
-        <Arrow />
-      </button>
+        <CarouselContent>
+          {recentPlays.map((item) => (
+            <CarouselItem key={item.playedAt} className="w-full">
+              <div className="p-4 mx-6 relative">
+                <Link
+                  href={item.url!}
+                  passHref
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title={`${item.title} by: ${item.artist}`}
+                  aria-label={`${item.title} by: ${item.artist}`}
+                >
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src={item.thumbnail}
+                    alt={item.title}
+                    className="w-full h-full sm:h-64 object-cover rounded-lg shadow-lg dark:shadow-zinc-900 brightness-[0.4]"
+                  />
+                  <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+                    <h3 className="text-xl font-semibold overflow-hidden text-ellipsis whitespace-nowrap">
+                      {item.title}
+                    </h3>
+                    <p className="text-gray-300 max-w-56 sm:max-w-full text-center overflow-hidden text-ellipsis whitespace-nowrap">
+                      {item.artist}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute top-1/2 left-0 bg-transparent hover:scale-125 transition-transform duration-300 -rotate-[90deg]" />
+        <CarouselNext className="absolute top-1/2 right-0 bg-transparent hover:scale-125 transition-transform duration-300 rotate-90" />
+      </Carousel>
     </div>
   );
 };
