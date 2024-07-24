1a363e0aa1ae4e453642c45795a015c0c0aba43b
Shahbaz <shahbazathwal2107@gmail.com>
2024-07-24 15:40:44 +0530

    cache revalidation

--- src/app/(app)/spotify/RenderedRecentlyPlayed.tsx
+++ src/app/(app)/spotify/RenderedRecentlyPlayed.tsx
@@ -1,10 +1,12 @@
 import { recentlyPlayed } from "@/app/actions/recentlyPlayed";
 import RecentlyPlayed from "@/components/RecentlyPlayed";
+import { revalidatePath } from "next/cache";
 
 export default async function RenderedRecentlyPlayed() {
 
     const data = await recentlyPlayed();
-  
+    revalidatePath('/')
+    
     return (
       <>
         <RecentlyPlayed recentPlays={data} />
