b869598e695ba8f370ad1e51d444023f9c926d13
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-05 23:14:22 +0530

    footer changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col text-sm items-center mt-32">
-      <Image
-        width={40}
-        height={40}
-        src="/logos/peace.svg"
-        alt="peace"
-        className="brightness-0 dark:invert w-14"
-      />
-      <p>&copy; {new Date().getFullYear()} - Shahbaz Singh</p>
+    <footer className="px-4 mt-28 w-full max-w-2xl border-t border-zinc-200 dark:border-zinc-700 pt-8 pb-5  text-zinc-700 dark:text-zinc-300 font-mono text-xs md:text-sm tracking-tight flex justify-between">
+      <p>
+        &copy; {new Date().getFullYear()} {"/"} Shahbaz Singh
+      </p>
+
+      <Link href="https://github.com/shahbaz-athwal/shahcodes">
+        View Source
+      </Link>
     </footer>
   );
 };
