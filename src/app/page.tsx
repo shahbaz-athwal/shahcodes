916daf2a0400b0e31bde463162af5fae927c3609
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-05 22:55:32 +0530

    ui improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,14 +51,16 @@ const Home = () => {
           I’m a full stack developer based in Canada
         </span>
         <p className="text-lg md:text-xl leading-normal dark:text-zinc-300/70">
-          I work with leading-edge companies to create exceptional products. I’m
-          currently working with MoonPay to help make web3 accessible. Before
-          that, I built personal and business banking products for millions of
-          customers at Monzo.
+          Junior year computer science student with nearly 2 years of
+          experience in Full Stack Development. Currently, I am focusing on
+          advanced Backend and DevOps skills. Proficient with{" "}
+          <strong>TypeScript</strong>, <strong>Next.js</strong>,{" "}
+          <strong>Node.js</strong>, <strong>Docker</strong>, and{" "}
+          <strong>AWS</strong>.
         </p>
         <Link
           href="/techstack"
-          className="group bg-zinc-950 dark:bg-zinc-50 dark:hover:bg-zinc-200 hover:bg-zinc-800 transition-colors inline-block mt-8 font-mono text-xs font-semibold rounded-full px-8 py-3 text-white dark:text-black"
+          className="group bg-zinc-950 dark:bg-zinc-50 dark:hover:bg-zinc-200 hover:bg-zinc-800 transition-colors inline-block mt-8 font-mono text-sm font-semibold rounded-full px-8 py-3 text-white dark:text-black"
         >
           More Information{" "}
           <span className="inline-block group-hover:translate-x-2 transition-transform">
@@ -68,7 +70,7 @@ const Home = () => {
       </section>
 
       <div>
-        <Header variant="primary" as="h2">
+        <Header variant="primary" as="h2" className="text-3xl">
           Projects
         </Header>
         <p className="text-lg pt-4 pb-8">
