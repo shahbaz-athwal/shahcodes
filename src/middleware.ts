ac6f835667bbbd1ea952a6f291a09a17cfa86c7b
Shahbaz <shahbazathwal2107@gmail.com>
2024-11-26 13:34:59 -0400

    middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,23 @@ import { NextRequest, NextResponse, userAgent } from "next/server";
 
 import { geolocation } from "@vercel/functions";
 
-export const config = {
-  matcher: "/",
-};
 
 export async function middleware(req: NextRequest) {
-  const { nextUrl: url } = req;
+  if (req.nextUrl.pathname === "/") {
+    const { nextUrl: url } = req;
 
-  const geo = geolocation(req);
+    const geo = geolocation(req);
 
-  const country = geo?.country || "CA";
-  const city = geo?.city || "Kentville";
-  const region = geo?.countryRegion || "NS";
-  const { isBot } = userAgent(req);
+    const country = geo?.country || "CA";
+    const city = geo?.city || "Kentville";
+    const region = geo?.countryRegion || "NS";
+    const { isBot } = userAgent(req);
 
-  url.searchParams.set("country", country);
-  url.searchParams.set("city", city);
-  url.searchParams.set("region", region);
-  url.searchParams.set("isBot", isBot.toString());
+    url.searchParams.set("country", country);
+    url.searchParams.set("city", city);
+    url.searchParams.set("region", region);
+    url.searchParams.set("isBot", isBot.toString());
 
-  return NextResponse.rewrite(url);
+    return NextResponse.rewrite(url);
+  }
 }
