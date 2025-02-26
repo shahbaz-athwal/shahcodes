46e16e53c337a41c9fe5ca6b85895023cc04eb51
Shahbaz Singh <shahbazathwal2107@gmail.com>
2025-02-26 19:38:32 -0400

    projects refactor

---
+++ src/app/(home)/Projects.tsx
@@ -0,0 +1,124 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Badge } from "../../components/ui/badge";
+import { Button } from "../../components/ui/button";
+import { IconCode, IconLink } from "@tabler/icons-react";
+
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  href: string;
+  code: string;
+  image: string;
+  badges?: string[];
+}
+
+const projects: {
+  href: string;
+  code: string;
+  title: string;
+  description: string;
+  image: string;
+  badges: string[];
+  hidden: boolean;
+}[] = [
+  {
+    title: "Whisperella",
+    href: "https://whisperella.shahcodes.in",
+    code: "https://github.com/shahbaz-athwal/whisperella",
+    description: "An Anonymous Messaging Platform",
+    image: "/projects/whisper.png",
+    badges: ["Next.js", "Auth.js", "TypeScript", "PostgreSQL", "Resend", "Zod"],
+    hidden: false,
+  },
+  {
+    title: "FindMyJob",
+    href: "https://findmyjob.shahcodes.in",
+    code: "https://github.com/shahbaz-athwal/find-my-job",
+    description: "A Two Way Job Application Portal",
+    image: "/projects/findmyjob.png",
+    badges: ["Express", "React", "MongoDB", "GCP Cloud Run", "Cloudinary"],
+    hidden: false,
+  },
+  {
+    title: "Portfolio/Blog",
+    href: "https://shahcodes.in/",
+    code: "https://github.com/shahbaz-athwal/shahcodes",
+    description: "My Personal Portfolio and Blog",
+    image: "/og.png",
+    badges: ["Next.js", "Tailwind", "TypeScript", "Redis", "Spotify API", "MDX"],
+    hidden: false,
+  },
+  {
+    title: "",
+    href: "",
+    code: "https://github.com/shahbaz-athwal/acadia-help",
+    description: "",
+    image: "/projects/acadia.png",
+    badges: ["Next.js", "Tailwind", "TypeScript", "PostgreSQL", "ShadCN", "Notion API", "Zod"],
+    hidden: true,
+  },
+];
+
+const ProjectCard = ({ title, description, href, code, image, badges }: ProjectCardProps) => {
+  return (
+    <div className="mx-2 flex h-full w-fit flex-col space-y-6 rounded-xl bg-zinc-100 p-8 hover:bg-zinc-200/70 dark:bg-stone-700/25 dark:hover:bg-stone-700/40 md:mx-0">
+      <div>
+        <h3 className="text-[17px] font-medium sm:text-lg">{title}</h3>
+        <p className="text-base dark:text-gray-300 sm:text-[17px]">{description}</p>
+      </div>
+      <Link href={href} target="_blank">
+        <div className="w-fit overflow-hidden rounded-lg shadow-lg dark:shadow-zinc-800/50">
+          <Image src={image} alt={title} width={700} height={400} className="aspect-[16/9] rounded-lg object-cover" />
+        </div>
+      </Link>
+      <div className="flex flex-wrap gap-2">
+        {badges?.map((badge) => (
+          <Badge
+            key={badge}
+            variant={"secondary"}
+            className="bg-zinc-200 font-normal hover:cursor-text dark:bg-zinc-700/70"
+          >
+            {badge}
+          </Badge>
+        ))}
+      </div>
+      <div className="flex gap-2">
+        <Link href={href} target="_blank">
+          <Button className="text-xs">
+            <IconLink className="mr-1 h-4 w-4" />
+            Live
+          </Button>
+        </Link>
+        <Link href={code} target="_blank">
+          <Button className="text-xs">
+            <IconCode className="mr-1 h-4 w-4" />
+            Code
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default function Projects() {
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:-ml-[20%] lg:w-[140%]">
+      {projects.map((project) => {
+        return (
+          !project.hidden && (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              href={project.href}
+              code={project.code}
+              badges={project.badges}
+              image={project.image}
+            />
+          )
+        );
+      })}
+    </div>
+  );
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,53 +1,6 @@
 import Link from "next/link";
 import { Header } from "@/components/ui/header";
-import ProjectCard from "@/components/ui/ProjectCard";
-
-const projects: {
-  href: string;
-  code: string;
-  title: string;
-  description: string;
-  image: string;
-  badges: string[];
-  hidden: boolean;
-}[] = [
-  {
-    title: "Whisperella",
-    href: "https://whisperella.shahcodes.in",
-    code: "https://github.com/shahbaz-athwal/whisperella",
-    description: "An Anonymous Messaging Platform",
-    image: "/projects/whisper.png",
-    badges: ["Next.js", "Auth.js", "TypeScript", "PostgreSQL", "Resend", "Zod"],
-    hidden: false,
-  },
-  {
-    title: "FindMyJob",
-    href: "https://findmyjob.shahcodes.in",
-    code: "https://github.com/shahbaz-athwal/find-my-job",
-    description: "A Two Way Job Application Portal",
-    image: "/projects/findmyjob.png",
-    badges: ["Express", "React", "MongoDB", "GCP Cloud Run", "Cloudinary"],
-    hidden: false,
-  },
-  {
-    title: "Portfolio/Blog",
-    href: "https://shahcodes.in/",
-    code: "https://github.com/shahbaz-athwal/shahcodes",
-    description: "My Personal Portfolio and Blog",
-    image: "/og.png",
-    badges: ["Next.js", "Tailwind", "TypeScript", "Redis", "Spotify API", "MDX"],
-    hidden: false,
-  },
-  {
-    title: "",
-    href: "",
-    code: "https://github.com/shahbaz-athwal/acadia-help",
-    description: "",
-    image: "/projects/acadia.png",
-    badges: ["Next.js", "Tailwind", "TypeScript", "PostgreSQL", "ShadCN", "Notion API", "Zod"],
-    hidden: true,
-  },
-];
+import Projects from "./Projects";
 
 export default function Home() {
   return (
@@ -78,24 +31,8 @@ export default function Home() {
         <p className="pb-8 pt-4 text-lg dark:text-zinc-300/70">
           Below is a selection of recent projects that I&apos;ve worked on.
         </p>
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:-ml-[20%] lg:w-[140%]">
-          {projects.map((project) => {
-            return (
-              !project.hidden && (
-                <ProjectCard
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  href={project.href}
-                  code={project.code}
-                  badges={project.badges}
-                  image={project.image}
-                />
-              )
-            );
-          })}
-        </div>
       </div>
+      <Projects />
     </>
   );
 }
