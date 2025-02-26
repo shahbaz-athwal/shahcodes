d4460688a097883b0b600a792cdc0f70b8d4e2a5
Shahbaz Singh <shahbazathwal2107@gmail.com>
2025-02-26 19:50:14 -0400

    better recently played fetching

--- src/app/spotify/RecentlyPlayed.tsx
+++ src/app/spotify/RecentlyPlayed.tsx
@@ -1,38 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
-import { SpotifyPlayedItem } from "@/types/SpotifyRecentlyPlayed";
 import Link from "next/link";
 import Image from "next/image";
-import { recentlyPlayed } from "@/actions/recentlyPlayed";
 import { TextGradient } from "@/components/ui/textgradient";
-import { Skeleton } from "@/components/ui/skeleton";
+import { useSpotify } from "@/hooks/useSpotify";
 
 const RecentlyPlayed = () => {
-  const [recentPlays, setRecentPlays] = useState<SpotifyPlayedItem[] | null>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await recentlyPlayed();
-      setRecentPlays(result);
-      setTimeout(() => setIsLoaded(true), 100);
-    };
-
-    fetchData();
-  }, []);
+  const { recentlyPlayed } = useSpotify();
 
   return (
-    <div className="relative overflow-hidden">
-      <h1 className="pb-6 text-3xl font-bold leading-tight">
-        <TextGradient variant="center">Recently Played</TextGradient>
-      </h1>
-      {recentPlays ? (
-        <Carousel
-          className={`w-full ${isLoaded ? "opacity-100 transition-opacity duration-700" : "opacity-0"}`}
-          plugins={[Autoplay({ delay: 4000 })]}
-        >
+    recentlyPlayed && (
+      <div className="relative overflow-hidden">
+        <h1 className="pb-6 text-3xl font-bold leading-tight">
+          <TextGradient variant="center">Recently Played</TextGradient>
+        </h1>
+        <Carousel className="w-full" plugins={[Autoplay({ delay: 4000 })]}>
           <div
             className="relative"
             style={{
@@ -40,7 +23,7 @@ const RecentlyPlayed = () => {
             }}
           >
             <CarouselContent>
-              {recentPlays.map((item, index) => (
+              {recentlyPlayed.map((item, index) => (
                 <CarouselItem key={index} className="w-full">
                   <div className="relative mx-6 p-4">
                     <Link
@@ -76,12 +59,8 @@ const RecentlyPlayed = () => {
           <CarouselPrevious className="absolute left-0 top-1/2 -rotate-[90deg] bg-transparent transition-transform duration-300 hover:scale-125" />
           <CarouselNext className="absolute right-0 top-1/2 rotate-90 bg-transparent transition-transform duration-300 hover:scale-125" />
         </Carousel>
-      ) : (
-        <div className="flex items-center justify-center py-4">
-          <Skeleton className="mx-10 h-64 w-[408px] rounded-lg sm:mx-10 sm:w-full" />
-        </div>
-      )}
-    </div>
+      </div>
+    )
   );
 };
 
--- src/app/spotify/SpotifyFetcher.tsx
+++ src/app/spotify/SpotifyFetcher.tsx
@@ -1,22 +1,34 @@
 "use client";
 
 import { currentlyPlaying } from "@/actions/currentlyPlaying";
+import { recentlyPlayed } from "@/actions/recentlyPlayed";
 import { useSpotify } from "@/hooks/useSpotify";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import useSWR from "swr";
 
 export default function SpotifyPrefetch() {
   const path = usePathname();
   const refreshInterval = path === "/spotify" ? 3 * 1000 : 10 * 1000;
 
-  const { setSpotifyListening } = useSpotify();
-  const fetcher = async () => {
+  const { setSpotifyListening, setSpotifyRecentlyPlayed } = useSpotify();
+
+  const currentlyPlayingFetcher = async () => {
     const data = await currentlyPlaying();
     setSpotifyListening(data);
     return data;
   };
 
-  useSWR("/spotify", fetcher, {
+  useEffect(() => {
+    const fetchRecentlyPlayed = async () => {
+      const data = await recentlyPlayed();
+      setSpotifyRecentlyPlayed(data);
+    };
+
+    fetchRecentlyPlayed();
+  }, [setSpotifyRecentlyPlayed]);
+
+  useSWR("/spotify", currentlyPlayingFetcher, {
     refreshInterval,
   });
 
