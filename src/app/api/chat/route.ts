ef39524b4e1264bc47ac847523f55e747eeb06a4
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-06-07 01:28:15 -0600

    add tool call

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,18 +1,41 @@
+import { getResource } from "@/lib/embeddings";
 import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { streamText, tool } from "ai";
+import { z } from "zod";
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+export const maxDuration = 45;
 
 const SECRET_SAUCE = process.env.SECRET_SAUCE;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const chatModel = openai("gpt-4o");
+
   const result = streamText({
     system: SECRET_SAUCE,
-    model: openai("o4-mini"),
+    model: chatModel,
     messages,
+    tools: {
+      // addResource: tool({
+      //   description: `Add a resource to your knowledge base. If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+      //   parameters: z.object({
+      //     content: z.string().describe("The content or resource to add to the knowledge base"),
+      //   }),
+      //   execute: async ({ content }) => {
+      //     return await createResource(content);
+      //   },
+      // }),
+      getResource: tool({
+        description: `Get a resource from knowledge base.`,
+        parameters: z.object({
+          content: z.string().describe("The content or resource to get from the knowledge base"),
+        }),
+        execute: async ({ content }) => {
+          return await getResource(content);
+        },
+      }),
+    },
   });
 
   return result.toDataStreamResponse();
